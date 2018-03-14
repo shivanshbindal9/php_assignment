2e88cb5ba741a5954c81d18762c2528f1aa4c0bf
First Name <you@example.com>
2018-03-14 16:41:04 +0530

    front end done

--- index.php
+++ index.php
@@ -2,12 +2,49 @@
 <html>
 <head>
 <title>
+PEOPLE SEARCH
 </title>
-<link href="./main.css" rel="stylesheet" type="text/css">
+
+<link href="main.css" rel="stylesheet" type="text/css">
+
+
 </head>
 <body>
-<form>
-<input type="text" value="hello">
+<img style="margin-left:40%" src="./images/index.jpeg" width="220px" height="220px">
+<br>
+<fieldset>
+<legend>SIGN IN </legend>
+<form action="validate.php" name="validation" method="post" onsubmit="checking()">
+<table>
+<tr>
+<td>
+Username:
+</td>
+<td>
+<input type="text" value="username" required >
+</td>
+</tr>
+<tr>
+<td>
+Password:
+</td>
+<td>
+<input type="password" value="password" required>
+</td>
+</tr>
+<tr>
+<td>
+<input type="submit" value="login" onsubmit="checking()">
+</td>
+
+</tr>
+</table>
 </form>
+</fieldset>
+<script type="text/javascript">
+var mail = /^[^@]+@[^@.]+\.[a-z]+$/i;
+function checking(){
+  alert("want to remember it");}
+</script>
 </body>
 </html>
